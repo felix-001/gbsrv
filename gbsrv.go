08fdd66911fc048ac33a6c06b5429a8963e1e5c9
rigensen <mr.right.0005@gmail.com>
2021-04-08 23:47:26 +0800

    update

--- gbsrv.go
+++ gbsrv.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strconv"
 
 	"github.com/jart/gosip/sip"
 )
@@ -31,6 +32,40 @@ func newConn() (*net.UDPConn, error) {
 	return conn, nil
 }
 
+func send200(msg *sip.Msg, conn *net.UDPConn, remoteAddr *net.UDPAddr) {
+	branch := ""
+	if msg != nil && msg.Via != nil && msg.Via.Param != nil {
+		param := msg.Via.Param.Get("branch")
+		if param != nil {
+			branch = param.Value
+		}
+	}
+	resp := &sip.Msg{
+		VersionMajor: 2,
+		VersionMinor: 0,
+		Status:       200,
+		Phrase:       "OK",
+		Via: &sip.Via{
+			Host: msg.Via.Host,
+			Port: msg.Via.Port,
+			Param: &sip.Param{
+				Name:  "rport",
+				Value: strconv.Itoa(int(msg.Via.Port)),
+				Next: &sip.Param{
+					Name:  "received",
+					Value: msg.Via.Host,
+					Next: &sip.Param{
+						Name:  "branch",
+						Value: branch,
+					},
+				},
+			},
+		},
+	}
+	log.Println("message is " + resp.String())
+	conn.WriteToUDP([]byte(resp.String()), remoteAddr)
+}
+
 func handleClient(conn *net.UDPConn) {
 	data := make([]byte, 1024)
 	_, remoteAddr, err := conn.ReadFromUDP(data)
@@ -42,10 +77,13 @@ func handleClient(conn *net.UDPConn) {
 	msg, err := sip.ParseMsg(data)
 	if err != nil {
 		log.Println(err)
-		return
+		//return
 	}
 	log.Println(msg)
-	conn.WriteToUDP([]byte("ok"), remoteAddr)
+	//if msg.Method == "Register" {
+	send200(msg, conn, remoteAddr)
+	return
+	//}
 }
 
 func main() {
